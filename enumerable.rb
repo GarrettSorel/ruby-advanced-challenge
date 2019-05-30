3f015a867af3c39267916fddc25cde63e0e8e7e4
GarrettSorel <garrettsorel@gmail.com>
2019-05-30 13:30:31 -0400

    enumerable update

---
+++ enumerable.rb
@@ -0,0 +1,30 @@
+module Enumerable
+
+  def my_each(arr)
+    for i in 0..self.length
+      yield(self[i])
+    end
+  end
+
+  def my_each_with_index
+  end
+
+  def my_select
+
+  end
+
+  def my_all
+  end
+
+  def my_none
+  end
+
+  def my_count
+  end
+
+  def my_map
+  end
+
+  def my_inject
+  end
+end
