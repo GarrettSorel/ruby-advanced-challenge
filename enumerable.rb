2535ffd5db5daae174416968ac82fcd1d93054d0
GarrettSorel <garrettsorel@gmail.com>
2019-06-07 11:41:44 -0400

    complete

--- enumerable.rb
+++ enumerable.rb
@@ -55,21 +55,42 @@ module Enumerable
   end
 
   def my_count
+    count = 0
+
+    for i in 0...self.length
+      count += 1 if yield(i)
+    end
+
+    puts count
   end
 
-  def my_map
+  def my_map(proc)
+    result = []
+
+    self.my_each do |e|
+      result << proc.call(e)
+    end
+
+    puts result
   end
 
-  def my_inject
+  def my_inject(total=nil)
+    for i in 0...self.length
+      total = yield(total, i)
+    end
+    puts total
   end
 
 end
 
 
-arr = [1,2,3,4,5]
+arr = [2,3,4,5]
+proc = Proc.new
 #arr.my_each { |x| puts x + 2 }
 #arr.my_each_with_index { |x, i| puts "#{i}: #{x}" }
 #arr.my_select { |x| x % 2 == 0}
 #arr.my_all { |x| x.is_a? Integer }
 #arr.my_any { |x| x % 2 == 0 }
-arr.my_none { |x| x.is_a? String}
+#arr.my_none { |x| x.is_a? String}
+#arr.my_count { |x| x.is_a? String }
+arr.my_map { |x| x + 5 }
